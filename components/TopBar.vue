8a63864e667e31aad65d91792d53d7fa891d066f
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2021-11-30 22:05:20 +0100

    Minor TopBar improvements.

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -4,17 +4,23 @@
       <b-row>
         <b-col col="6" sm="9" md="8">
           <div class="d-none d-sm-inline mr-3">
-            <b-icon-map-fill variant="primary" scale="0.75"/>
-            <a href="https://g.page/DentystanaWoli" target="_blank">ul. Wolska 81, Warszawa, Wola</a>
+            <a href="https://g.page/DentystanaWoli" target="_blank">
+              <b-icon-map-fill variant="primary" scale="0.75"/>
+              <span>ul. Wolska 81, Warszawa, Wola</span>
+            </a>
           </div>
           <div class="d-inline">
-            <b-icon-telephone-fill variant="primary" scale="0.75"/>
-            <a href="tel:+48730880881">730 880 881</a>
+            <a href="tel:+48730880881">
+              <b-icon-telephone-fill variant="primary" scale="0.75"/>
+              <span>730 880 881</span>
+            </a>
           </div>
         </b-col>
         <b-col col="6" sm="3" md="4" class="text-right">
-          <b-icon-facebook variant="primary" scale="0.75"/>
-          <a href="https://www.facebook.com/Ni-w-z%C4%85b-406039073175052" target="_blank">@niwzab</a>
+          <a href="https://www.facebook.com/Ni-w-z%C4%85b-406039073175052" target="_blank">
+            <b-icon-facebook variant="primary" scale="0.75"/>
+            <span>@niwzab</span>
+          </a>
         </b-col>
       </b-row>
     </b-container>
