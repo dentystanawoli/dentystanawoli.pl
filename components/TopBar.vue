aa69b7d17c06a18fd40ab022804c6adf2e14fced
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2021-11-30 01:48:23 +0100

    Create TopBar component with styles.

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="top-bar-wrapper">
-    Top bar
+    <b-container>
+      <b-row>
+        <b-col col="6" sm="9" md="8">
+          <div class="d-none d-sm-inline mr-3">
+            <b-icon-map-fill variant="primary" scale="0.75"/>
+            <a href="https://g.page/DentystanaWoli" target="_blank">ul. Wolska 81, Warszawa, Wola</a>
+          </div>
+          <div class="d-inline">
+            <b-icon-telephone-fill variant="primary" scale="0.75"/>
+            <a href="tel:+48730880881">730 880 881</a>
+          </div>
+        </b-col>
+        <b-col col="6" sm="3" md="4" class="text-right">
+          <b-icon-facebook variant="primary" scale="0.75"/>
+          <a href="https://www.facebook.com/Ni-w-z%C4%85b-406039073175052" target="_blank">@niwzab</a>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
