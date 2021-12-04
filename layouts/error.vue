3d48866351fd5e531ea4e9384c76df78d06b8749
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2021-12-05 00:06:14 +0100

    Create error layout with dependencies.

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container mt-5 mb-5">
+    <div class="row">
+      <div class="col">
+        <h2 class="text-primary text-center ">Ups...</h2>
+        <p class="text-center">{{ error.message }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: {
+    error: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+  },
+  head() {
+    return {
+      title: this.error.statusCode,
+    }
+  },
+  mounted () {
+    this.$store.commit('SET_TITLE', this.error.statusCode)
+  }
+}
+</script>
