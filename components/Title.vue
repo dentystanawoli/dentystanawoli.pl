a76bf5dccc65c72c9ffa38a4052acbc3bd8c2ae9
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2021-12-05 00:04:56 +0100

    Create Title component with styles and default Vuex store.

--- components/Title.vue
+++ components/Title.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="title-wrapper">
-    Title
+  <div v-if="title" class="title-wrapper gradient pt-5 pb-5 text-center">
+    <b-container fluid>
+      <b-row>
+        <b-col cols="12">
+          <h1 class="text-white">{{ title }}</h1>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    ...mapState({
+      title: state => state.title
+    })
+  }
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,9 @@
+export const state = {
+  title: false
+}
+
+export const mutations = {
+  SET_TITLE (state, title) {
+    state.title= title
+  }
+}
