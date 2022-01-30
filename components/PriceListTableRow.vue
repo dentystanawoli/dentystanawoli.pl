5fdffe4977d8bb258419ad7c0d1fa55705e367b2
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-01-31 00:50:11 +0100

    Add &nbsp; tag.

--- components/PriceListTableRow.vue
+++ components/PriceListTableRow.vue
@@ -1,8 +1,8 @@
 <template>
-    <tr>
-      <td :class="`level-${row.level || 0}`" :colspan="row.price? 1 : 2" v-html="row.item"></td>
-      <td class="text-right" v-if="row.price">{{ row.price }} zł</td>
-    </tr>
+  <tr>
+    <td :class="`level-${row.level || 0}`" :colspan="row.price? 1 : 2" v-html="row.item"></td>
+    <td v-if="row.price" class="text-right">{{ row.price.replace(/\ - /g, '&nbsp;-&nbsp;') }}&nbsp;zł</td>
+  </tr>
 </template>
 
 <script>
