982d55c552b3cc473cf4f4c1214a7e3d6ef0b933
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-02-16 01:03:30 +0100

    Create BlogImage component.

---
+++ components/BlogImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-center blog-image mb-3 mt-3">
+    <img :src="imgSrc()" :alt="alt" class="img-fluid"/>
+    <figcaption :v-if="caption">{{ caption }}</figcaption>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    },
+    caption: {
+      type: String,
+      default () {
+        return ''
+      }
+    }
+  },
+  methods: {
+    imgSrc () {
+      try {
+        return require(`~/assets/images/blog/${this.src}`)
+      } catch (error) {
+        return null
+      }
+    }
+  }
+}
+</script>
