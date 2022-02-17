bea885f68d567b0a2021794aa95fe4e3a07947d7
marcinmaruszewski <marcinmaruszewski@users.noreply.github.com>
2022-02-17 21:29:43 +0000

    deploy: 15d34e97152b8196232890c86fc1caeacca58a3a

--- _nuxt/static/1645132983/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645132983",layout:"default",error:a,state:{title:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"979dd494"}}}}(null,"\u002F"));
