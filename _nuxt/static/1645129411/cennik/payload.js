4891c4e64e591f8a6db46bc93594d960c4a2df12
marcinmaruszewski <marcinmaruszewski@users.noreply.github.com>
2022-02-17 20:23:35 +0000

    deploy: cdf7f19e1e493a5965ffd75e468a8babb157584c

---
+++ _nuxt/static/1645129411/cennik/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/cennik", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1645129411/pytania-i-odpowiedzi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645129411",layout:"default",error:a,state:{title:false},serverRendered:true,routePath:"\u002Fpytania-i-odpowiedzi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d5f71b7"}}}}(null));
