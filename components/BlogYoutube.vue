15e29aff451016c5f72ae0c719c308093de7d756
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-03-23 23:00:30 +0100

    Create BlogYoutube component.

---
+++ components/BlogYoutube.vue
@@ -0,0 +1,29 @@
+<template>
+  <p :class="['blog-youtube', 'embed-responsive', `embed-responsive-${ratio}`]">
+    <iframe
+      width="560"
+      height="315"
+      :src="`https://www.youtube-nocookie.com/embed/${video}`"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </p>
+</template>
+
+
+<script>
+export default {
+  props: {
+    video: {
+      type: String,
+      required: true
+    },
+    ratio: {
+      type: String,
+      required: false,
+      default: '16by9'
+    }
+  }
+}
+</script>
