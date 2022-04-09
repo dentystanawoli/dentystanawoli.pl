a15d67004253edbaaefd17dbf27ed3f8fba1eb47
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-04-09 22:54:53 +0200

    Display LatestBlogs on homepage.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1 class="text-center">lorem ipsum</h1>
+  <div>
+    <h1 class="text-center">lorem ipsum</h1>
+    <LatestBlogs :blogs="blogs"/>
+  </div>
 </template>
 
 <script>
+import LatestBlogs from '@/components/Homepage/LatestBlogs'
+
 export default {
-  head() {
+  components: {LatestBlogs},
+  async asyncData ({$content}) {
+    const blogs = await $content('blog').sortBy('createdAt', 'desc').only(['title', 'excerpt', 'createdAt', 'image', 'category', 'body']).limit(3).fetch()
+    return {blogs}
+  },
+  data () {
+    return {
+      blogs: {}
+    }
+  },
+  head () {
     return {
       titleTemplate: '%s',
       title: 'Dentysta na Woli'
