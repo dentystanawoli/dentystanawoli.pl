e79523c311d13226dca3f55b7d7009da67f6510b
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-04-11 23:37:54 +0200

    Create Homepage\Service component with dependencies.

---
+++ components/Homepage/Services.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container services mb-5 mt-5">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="text-center header-underline mb-5">Nasze usługi</h2>
+      </div>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
+      <ServiceCard v-for="service in services" :key="service.slug" :service="service"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    services: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
     <h1 class="text-center">lorem ipsum</h1>
+    <Services :services="services"/>
     <LatestBlogs :blogs="blogs"/>
   </div>
 </template>
 
 <script>
 import LatestBlogs from '@/components/Homepage/LatestBlogs'
+import Services from '@/components/Homepage/Services'
 
 export default {
-  components: {LatestBlogs},
+  components: {Services, LatestBlogs},
   async asyncData ({$content}) {
     const blogs = await $content('blog').sortBy('createdAt', 'desc').only(['title', 'excerpt', 'createdAt', 'image', 'category', 'body']).limit(3).fetch()
-    return {blogs}
+    const services = await $content('oferta').sortBy('weight').fetch()
+    return {blogs, services}
   },
   data () {
     return {
-      blogs: {}
+      blogs: {},
+      services: {}
     }
   },
   head () {
