93b0ff48655de497fdd5bfe8b034a062225fb6e7
Marcin Maruszewski <kontakt@marcinmaruszewski.me>
2022-04-12 00:07:57 +0200

    Fix typo.

--- components/Homepage/Hero.vue
+++ components/Homepage/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hero gradient mt-5 mb-5 p-5">
-    <h1 class="mt-5">Czy wiesz, że przegląd dentstyczny powinno wykonywać się co pół roku?</h1>
+    <h1 class="mt-5">Czy wiesz, że przegląd dentystyczny powinno wykonywać się co pół roku?</h1>
     <nuxt-link to="/kontakt" class="btn btn-primary mt-5 mb-5">Zapisz się teraz</nuxt-link>
   </div>
 </template>
